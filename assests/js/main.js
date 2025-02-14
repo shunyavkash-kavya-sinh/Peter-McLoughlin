aedeff1fd8db0390717237e6e327423fa71eae13
Sinh Kavya <kavya.singh@shunyavkash.com>
2025-02-14 17:29:31 +0530

    done

---
+++ assests/js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $(".toggle-menu").click(function () {
+      $(".menu").toggleClass("active");
+  });
+});
+
+$(".testimonial-slider").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
